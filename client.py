006c06e2d90383fdf26567543629143c98296224
matteogrechi <matteogrechi98@gmail.com>
2022-02-18 15:15:33 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import wx  # pip3 install wxPython
 import cv2 # pip3 install opencv-python
+import socket
 
 # The following code is taken from 
 # https://stackoverflow.com/questions/35009984/get-stream-from-webcam-with-opencv-and-wxpython
@@ -8,7 +9,9 @@ class viewWindow(wx.Frame):
         wx.Panel.__init__(self, parent, wx.ID_ANY)
         self.pnl = wx.Panel(self)
         # Connecting to the camera
-        self.capture = cv2.VideoCapture("rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4") # "rtsp://192.168.1.10:554/livestream/12")
+        #videoStream = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4"
+        videoStream = "rtsp://192.168.1.10:554/livestream/12"
+        self.capture = cv2.VideoCapture(videoStream)
         ret, self.frame = self.capture.read()
         if ret:
             self.wSize = self.frame.shape[:2]
@@ -26,14 +29,37 @@ class viewWindow(wx.Frame):
         else:
             print("Error no webcam image")
         
+        # Keyboard
         self.pnl.Bind(wx.EVT_KEY_DOWN, self.keyDown)
         self.pnl.Bind(wx.EVT_KEY_UP, self.keyUp)
-        self.kd = False
+        self.kd = None
+        self.keyDict = {
+                72 : "H",
+                74 : "J",
+                75 : "K",
+                76 : "L",
+                70 : "F"
+        }
+        self.action = {
+            "H" : {"on" : "", "off" : ""},
+            "J" : {"on" : "", "off" : ""},
+            "K" : {"on" : "", "off" : ""},
+            "L" : {"on" : "", "off" : ""},
+            "F" : {"on" : "fe:04:07:ff:be:aa:64:00:00:00:f5:81", "off" : "fe:04:45:ff:be:aa:00:00:00:00:cf:70"}
+        }
 
+        # Command transmission connection
+        cmdSocketAddress = ["192.168.1.12", 20002]
+        self.commandConnection = socket.create_connection(cmdSocketAddress)
+
+        #
         self.pnl.SetSizer(self.vbox)
         self.vbox.Fit(self)
         self.Show()
         #self.pnl.SetFocus()
+
+    def __del__(self):
+        self.commandConnection.close()
 
     def redraw(self,e):
         ret, self.frame = self.capture.read()
@@ -44,18 +70,23 @@ class viewWindow(wx.Frame):
             self.Refresh()
     
     def keyDown(self, e):
-        #self.Destroy()
-        if not(self.kd):
-            print("*")
-        self.kd = True
+        if self.kd == None:
+            key = e.GetKeyCode()
+            self.kd = self.keyDict.get(key,None)
+            self.sendCommand(self.action[self.kd]["on"])
+        
         
        # box = wx.MessageDialog(None, 'Do you like this blog?', 'Titolo box',wx.YES_NO) #Il box appare con due possibili scelte, Yes o No, in alternativa potete utilizzare il parametro wx.OK apparirà un'unica scelta, quella di pigiare il tasto OK.
         #answer=box.ShowModal() #Nel caso di parametro wx.YES_NO, la risposta al quesito visualizzato (appunto si o no) verrà salvata in questo caso nella variabile answer. 
         #box.Destroy()
     
     def keyUp(self, e):
-        self.Destroy()
-        self.kd = False
+        self.sendCommand(self.action[self.kd]["off"])
+        self.kd = None
+
+    def sendCommand(self, command):
+        myBytes = bytes.fromhex(command.replace(":"," "))
+        self.commandConnection.send(myBytes)
 
 def main():
     app = wx.App()
